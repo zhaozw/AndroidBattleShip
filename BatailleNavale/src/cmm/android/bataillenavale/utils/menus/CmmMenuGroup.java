4f94a447ac90dbc549ee01d9d14b7216f94ba271
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-02-20 10:03:44 +0100

    add font for menuGroup

--- BatailleNavale/src/cmm/android/bataillenavale/utils/menus/CmmMenuGroup.java
+++ BatailleNavale/src/cmm/android/bataillenavale/utils/menus/CmmMenuGroup.java
@@ -3,17 +3,18 @@ package cmm.android.bataillenavale.utils.menus;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class CmmMenuGroup extends Sprite {
 	private ArrayList<CmmMenu> menus;
 	protected float menuHeight;
 	private TextureRegion menuText;
-	private BitmapFont font; //TODO initialize
+	private BitmapFont font;
 	
 	public CmmMenuGroup(ArrayList<CmmMenu> menus, TextureRegion menuText, float x, float y, float width, float height) {
 		super();
@@ -24,9 +25,12 @@ public class CmmMenuGroup extends Sprite {
 		setSize(width, height);
 		
 		menuHeight = height / menus.size();
-		
-		CmmMenuGroupListener listener = new CmmMenuGroupListener(this);
-		Gdx.input.setInputProcessor(listener);
+
+		/* ***** génère une police à partir d'un fichier TTF ***** */
+		FileHandle fontFile = Gdx.files.local("data/fonts/mainMenu.ttf");
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFile);
+		font = generator.generateFont(15);
+		generator.dispose();
 	}
 	
 	public CmmMenu getMenuAt(int index) {
@@ -39,12 +43,18 @@ public class CmmMenuGroup extends Sprite {
 	
 	public void draw(SpriteBatch batch) {
 		float x = getX();
+		float y = getY() + getHeight();
+		float width = getWidth();
 		String menuName;
 		for(CmmMenu m: menus) {
 			menuName = m.getName();
-			x -= menuHeight;
-			batch.draw(menuText, x, this.getY(), this.getWidth(), menuHeight);
-			font.draw(batch, menuName, x, this.getY());
+			y -= menuHeight;
+			batch.draw(menuText, x, y, width, menuHeight);
+			font.draw(batch, menuName, x, y);
 		}
+	}
+
+	public void setFont(BitmapFont font) {
+		this.font = font;
 	}
 }
