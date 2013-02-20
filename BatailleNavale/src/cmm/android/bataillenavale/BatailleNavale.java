312d5259cc56087aff2ad623c6021222fbb6ced7
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-02-20 10:02:00 +0100

    add change screen system for Game

--- BatailleNavale/src/cmm/android/bataillenavale/BatailleNavale.java
+++ BatailleNavale/src/cmm/android/bataillenavale/BatailleNavale.java
@@ -1,15 +1,22 @@
 package cmm.android.bataillenavale;
 
+import java.util.ArrayList;
+
 import cmm.android.bataillenavale.utils.CmmScreenAdapter;
+import cmm.android.bataillenavale.view.screens.CmmFinalScreen;
+import cmm.android.bataillenavale.view.screens.MainMenuScreen;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BatailleNavale extends Game {
+	public static final int MAIN_MENU = 0, CMM_FINAL = 1;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+	private ArrayList<Screen> screens;
 	
 	@Override
 	public void create() {		
@@ -20,6 +27,12 @@ public class BatailleNavale extends Game {
 		batch = new SpriteBatch();
 		
 		CmmScreenAdapter.setSpriteBatch(batch);
+		CmmScreenAdapter.setCamera(camera);
+		
+		screens = new ArrayList<Screen>();
+		screens.add(MAIN_MENU, new MainMenuScreen(this));
+		screens.add(CMM_FINAL,new CmmFinalScreen(this));
+		setScreen(screens.get(MAIN_MENU));
 	}
 
 	@Override
@@ -34,4 +47,8 @@ public class BatailleNavale extends Game {
 	public void pause() {}
 	@Override
 	public void resume() {}
+	
+	public void setScreen(int screenType) {
+		setScreen(screens.get(screenType));
+	}
 }
