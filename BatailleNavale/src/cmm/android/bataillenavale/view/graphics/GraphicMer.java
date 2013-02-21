f33162b92327ec1e9af3af8cba21c3a10f6051f9
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-02-21 13:23:28 +0100

    create a graphic class for Sea

--- BatailleNavale/src/cmm/android/bataillenavale/view/graphics/GraphicMer.java
+++ BatailleNavale/src/cmm/android/bataillenavale/view/graphics/GraphicMer.java
@@ -1,5 +1,29 @@
 package cmm.android.bataillenavale.view.graphics;
 
-public class GraphicMer {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import cmm.android.bataillenavale.modele.Coord2D;
+import cmm.android.bataillenavale.modele.Mer;
+
+public class GraphicMer extends Sprite {
+	private Mer mer;
+	
+	public GraphicMer(Mer mer) {
+		this.mer = mer;
+	}
+
+	public Mer getMer() {
+		return mer;
+	}
+
+	
+	public Coord2D getCaseAt(float screenX, float screenY) {
+		if(getBoundingRectangle().contains(screenX, screenY)) {
+			int x = (int)( (screenX - getX()) / (getWidth() / Mer.ARRAY_SIZE) );
+			int y = (int)( (screenY - (getY() + getHeight())) / (getHeight() / Mer.ARRAY_SIZE) );
+			return new Coord2D(x, y);
+		}
+		return null;
+	}
 	 
 }
