e30d8c4116c2ab96240b508e07e28a791049b6ec
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-03-11 13:16:01 +0100

    add flag "missed" when you shoot on water

--- BatailleNavale/src/cmm/android/bataillenavale/modele/Mer.java
+++ BatailleNavale/src/cmm/android/bataillenavale/modele/Mer.java
@@ -3,7 +3,7 @@ package cmm.android.bataillenavale.modele;
 import java.util.ArrayList;
 
 public class Mer {
-	public static final int EMPTY = 0, BOAT_HANDLE_GOOD = 1, BOAT_HANDLE_KILLED = 2, TOUCHED = 3, MISSED = 4;
+	public static final int EMPTY = 0, BOAT_HANDLE_GOOD = 1, BOAT_HANDLE_KILLED = 2, MISSED = 3;
 	public static final int ARRAY_SIZE = 8;
 	public static final int NB_BOAT_HANDLES = 1 + 2 + 3 + 4 + 5;
 	private int[][] mer;
@@ -27,7 +27,9 @@ public class Mer {
 			nbHandlesTouched++;
 			return true;
 		}
-
+		
+		//else
+		mer[y][x] = MISSED;
 		return false;
 	}
 
