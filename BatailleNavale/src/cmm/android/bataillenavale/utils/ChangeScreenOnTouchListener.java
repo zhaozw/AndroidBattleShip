12815df224e5cec5a667d27dbe95334fce516e6e
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-03-20 10:46:15 +0100

    add a new Listener helper which change screen when on touch

---
+++ BatailleNavale/src/cmm/android/bataillenavale/utils/ChangeScreenOnTouchListener.java
@@ -0,0 +1,24 @@
+package cmm.android.bataillenavale.utils;
+
+import com.badlogic.gdx.InputAdapter;
+
+public class ChangeScreenOnTouchListener extends InputAdapter {
+	private CmmScreenAdapter screen;
+	
+	public ChangeScreenOnTouchListener(CmmScreenAdapter screen) {
+		this.screen = screen;
+	}
+	
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		onChangeScreen();
+		screen.getApp().setScreen(screen);
+		return true;
+	}
+	
+	/**
+	 * Méthode appelé lorsqu'on change de screen, c'est-à-dire lorsqu'on clique sur l'ecran
+	 * Par défaut, elle ne fait rien, mais on peut hériter cette classe afin de réimplémenter cette méthode.
+	 */
+	public void onChangeScreen() {}
+}
