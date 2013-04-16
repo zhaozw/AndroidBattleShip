53a82d25c6a8b03b3e349f5764718d5e8fbb205a
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-04-16 12:56:28 +0200

    write the server

--- BatailleNavaleServer/src/Main.java
+++ BatailleNavaleServer/src/Main.java
@@ -2,22 +2,24 @@ import java.io.IOException;
 
 import com.esotericsoftware.kryonet.Server;
 
-
 public class Main {
+	public static int UDP_PORT = 54723, TCP_PORT = 54722;
 	public static void main(String args[]) {
-		/* ***** Récupération du port ***** */
-		int port = 54722;
+		/* ***** Récupération du port ***** */		
 		if(args.length > 0) {
-			port = Integer.parseInt(args[0]);
+			TCP_PORT = Integer.parseInt(args[0]);
+			UDP_PORT = TCP_PORT + 1;
 		}
+		System.out.println("" + UDP_PORT + " - " + TCP_PORT);
 		
 		/* ***** lancement du serveur ***** */
 		Server server = new Server();
 		server.start();
+		
 		try {
-			server.bind(port, port);
+			server.bind(TCP_PORT, UDP_PORT);
 		} catch (IOException e) {
-			System.out.println("Impossible de placer le serveur sur " + port);
+			System.out.println("Impossible de placer le serveur sur " + TCP_PORT);
 			e.printStackTrace();
 			System.exit(1);
 		}
