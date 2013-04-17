e5332948f4b8065186e0e61caae02e279f844110
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-04-17 12:54:23 +0200

    create a menu to choose AI level

--- BatailleNavale/src/cmm/android/bataillenavale/view/screens/IaMenuScreen.java
+++ BatailleNavale/src/cmm/android/bataillenavale/view/screens/IaMenuScreen.java
@@ -8,79 +8,87 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import cmm.android.bataillenavale.BatailleNavale;
+import cmm.android.bataillenavale.modele.ia.IntelligenceArtificielleDifficile;
+import cmm.android.bataillenavale.modele.ia.IntelligenceArtificielleFacile;
+import cmm.android.bataillenavale.modele.ia.IntelligenceArtificielleMoyen;
 import cmm.android.bataillenavale.utils.CmmScreenAdapter;
 import cmm.android.bataillenavale.utils.menus.CmmMenu;
 import cmm.android.bataillenavale.utils.menus.CmmMenuGroup;
 import cmm.android.bataillenavale.utils.menus.CmmMenuGroupListener;
 
-/**
- * Screen permettant d'afficher un menu afin de choisir ce que l'on veut faire parmi:
- * 		Mode 1 joueur,
- * 		Mode réseau,
- * 		Options,
- * 		Quitter.
- * @author Jonathan GEOFFROY, Samy CHAYEM
- * @version 1.0
- */
-public class MainMenuScreen extends CmmScreenAdapter {
+public class IaMenuScreen extends CmmScreenAdapter {
 	private CmmMenuGroup menu;
-	
-	public MainMenuScreen(BatailleNavale app) {
+
+	public IaMenuScreen(BatailleNavale app) {
 		super(app, false);
 	}
-
+	public IaMenuScreen(BatailleNavale app, boolean continousRendering) {
+		super(app, continousRendering);
+	}
 	@Override
 	public void initialize() {
 		super.initialize();
+
 		/* *****  Création du fond ***** */
 		Texture wallText = new Texture("data/img/mainMenuWallpaper.jpg");
 		TextureRegion wallTextReg = new TextureRegion(wallText);
 		Sprite wallpaper = new Sprite(wallTextReg);
 		setWallpaper(wallpaper);
 		textures.add(wallText);
-		
+
 		/* ***** Création des boutons composants le menu ***** */
 		ArrayList<CmmMenu> menus = new ArrayList<CmmMenu>();
-		menus.add(new CmmMenu("1 joueur") {
+		menus.add(new CmmMenu("Facile") {
 			@Override
 			public boolean onTouched() {
-				app.setScreen(BatailleNavale.IA_MENU);
+				VersusComputerGameScreen computerGame = (VersusComputerGameScreen) app.getScreen(BatailleNavale.VERSUS_COMPUTER_GAME);
+				computerGame.setIa(new IntelligenceArtificielleFacile());
+
+				app.setScreen(BatailleNavale.PLACE_BATEAU);
 				return true;
 			}
 		});
-		menus.add(new CmmMenu("2 joueurs") {
+		menus.add(new CmmMenu("Moyen") {
 			@Override
 			public boolean onTouched() {
-				app.setScreen(BatailleNavale.SEARCH_ENNEMY);
+				VersusComputerGameScreen computerGame = (VersusComputerGameScreen) app.getScreen(BatailleNavale.VERSUS_COMPUTER_GAME);
+				computerGame.setIa(new IntelligenceArtificielleMoyen());
+
+				app.setScreen(BatailleNavale.PLACE_BATEAU);
 				return true;
 			}
 		});
-		menus.add(new CmmMenu("Options") {
+		menus.add(new CmmMenu("Difficile") {
 			@Override
 			public boolean onTouched() {
-				Gdx.app.log("TMP_batailleNavale", "on change les options!");
+				VersusComputerGameScreen computerGame = (VersusComputerGameScreen) app.getScreen(BatailleNavale.VERSUS_COMPUTER_GAME);
+				computerGame.setIa(new IntelligenceArtificielleDifficile());
+
+				app.setScreen(BatailleNavale.PLACE_BATEAU);
 				return true;
 			}
 		});
-		menus.add(new CmmMenu("Quitter") {
+		menus.add(new CmmMenu("Retour") {
 			@Override
 			public boolean onTouched() {
-				app.setScreen(BatailleNavale.CMM_FINAL);
+				app.setScreen(BatailleNavale.MAIN_MENU);
 				return true;
 			}
 		});
-		
+
 		/* ***** Création de la texture du menu ***** */
 		Texture menuText = new Texture("data/img/mainMenu.png");
 		TextureRegion menuTextReg = new TextureRegion(menuText);
 		textures.add(menuText);
-		
+
 		/* ***** Création du menu ***** */
 		menu = new CmmMenuGroup(menus, menuTextReg, -0.4f, -0.4f, 0.8f, 0.8f);
 		sprites.add(menu);
-		
+
 		/* ***** Création du listener sur le menu ***** */
 		CmmMenuGroupListener listener = new CmmMenuGroupListener(menu);
 		Gdx.input.setInputProcessor(listener);
 	}
+
+
 }
